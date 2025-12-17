7858d2b23f31cd16f42b7f38548af66419caf186
MaxWutw <WuMax13@gmail.com>
2025-12-18 02:31:32 +0800

    commit

---
+++ edr/Program.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+using Microsoft.Diagnostics.Tracing.Session;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EDRPOC
+{
+    internal class Program
+    {
+        const string SECRET = "wwfZJQ7nU0GIYcQGcSxO8npXCRTZaFzi";
+
+        // Dictionary to store process ID to executable filename mapping
+        private static Dictionary<int, string> processIdToExeName = new Dictionary<int, string>();
+
+        // Flag to ensure the answer is sent only once
+        private static bool answerSent = false;
+
+        static async Task Main(string[] args)
+        {
+            using (var kernelSession = new TraceEventSession(KernelTraceEventParser.KernelSessionName))
+            {
+                Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e) { kernelSession.Dispose(); };
+                Console.WriteLine("Listening for events... Press Ctrl+C to exit.");
+
+                kernelSession.EnableKernelProvider(
+                    KernelTraceEventParser.Keywords.ImageLoad |
+                    KernelTraceEventParser.Keywords.Process |
+                    KernelTraceEventParser.Keywords.DiskFileIO |
+                    KernelTraceEventParser.Keywords.FileIOInit |
+                    KernelTraceEventParser.Keywords.FileIO
+                );
+
+                kernelSession.Source.Kernel.ProcessStart += processStartedHandler;
+                kernelSession.Source.Kernel.ProcessStop += processStoppedHandler;
+                kernelSession.Source.Kernel.FileIORead += fileReadHandler;
+
+                kernelSession.Source.Process();
+            }
+        }
+
+        private static async void processStartedHandler(ProcessTraceData data)
+        {
+            lock (processIdToExeName)
+            {
+                processIdToExeName[data.ProcessID] = data.ImageFileName;
+            }
+
+            if (data.ImageFileName.ToLower() == "cmd.exe")
+            {
+                Console.WriteLine($"[+] Detected cmd.exe process start. PID: {data.ProcessID}, Command Line: {data.CommandLine}");
+                string sensitiveFileKeyword1 = "copy", sensitiveFileKeyword2 = "login data";
+
+                if (!string.IsNullOrEmpty(data.CommandLine) && 
+                    data.CommandLine.ToLower().Contains(sensitiveFileKeyword1) &&
+                    data.CommandLine.ToLower().Contains(sensitiveFileKeyword2))
+                {
+                    Console.WriteLine($"[!] Detected cmd.exe process with suspicious command line: {data.CommandLine}");
+                    string parentExeName = null;
+                    int PPID = data.ParentID;
+
+
+                    if (!answerSent && PPID != 0)
+                    {
+                        lock (processIdToExeName)
+                        {
+                            processIdToExeName.TryGetValue(PPID, out parentExeName);
+                        }
+                        Console.WriteLine($"[+] Process: {data.ProcessName} (PID: {data.ProcessID})");
+                        Console.WriteLine($"[+] Parent: {parentExeName} (PPID: {PPID})");
+
+                        if (parentExeName == null) return;
+                        await SendAnswerToServer(JsonConvert.SerializeObject(
+                            new
+                            {
+                                answer = parentExeName,
+                                secret = SECRET
+                            }
+                        ));
+                        answerSent = true;
+                    }
+                }
+            }
+        }
+
+        private static void processStoppedHandler(ProcessTraceData data)
+        {
+            lock (processIdToExeName)
+            {
+                processIdToExeName.Remove(data.ProcessID);
+            }
+        }
+
+        private static async void fileReadHandler(FileIOReadWriteTraceData data)
+        {
+            // Check if the answer has already been sent
+            if (answerSent) return;
+
+            // Define the full path to the target file
+            string targetFilePath = ("C:\\Users\\bombe\\AppData\\Local\\bhrome\\Login Data").ToLower();
+
+            if (data.FileName.ToLower().Equals(targetFilePath))
+            {
+                string exeName = null;
+                lock (processIdToExeName)
+                {
+                    processIdToExeName.TryGetValue(data.ProcessID, out exeName);
+                }
+
+                if (exeName == null || !exeName.StartsWith("BOMBE")) return;
+
+                Console.WriteLine("File read: {0}, process: {1} with pid {2}, exe: {3}", data.FileName, data.ProcessName, data.ProcessID, exeName);
+
+                // Send the executable filename to the server
+                if (!string.IsNullOrEmpty(exeName))
+                {
+                    await SendAnswerToServer(JsonConvert.SerializeObject(
+                        new
+                        {
+                            answer = exeName,
+                            secret = SECRET
+                        }
+                    ));
+
+                    // Set the flag to true to disable further handling
+                    answerSent = true;
+                }
+            }
+        }
+
+        private static async Task SendAnswerToServer(string jsonPayload)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    HttpResponseMessage response = await client.PostAsync("https://submit.bombe.top/submitEdrAns", content);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Response: {responseBody}");
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+        }
+    }
+}
